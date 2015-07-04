2949912633c1cbc45c5ed9c323e7d2e836f7f5c8
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-07-04 23:00:28 +0900

    Added closure to gulpfile.

--- plugin/src/main/resources/gulpfile.js
+++ plugin/src/main/resources/gulpfile.js
@@ -1,31 +1,33 @@
-var fs = require('fs');
-var uglify = require('gulp-uglify');
-var mainBowerFiles = require('main-bower-files');
-var coffee = require('gulp-coffee');
-var less = require('gulp-less');
-var cssmin = require('gulp-minify-css');
-var gulp = require('gulp');
+(function() {
+    var fs = require('fs');
+    var uglify = require('gulp-uglify');
+    var mainBowerFiles = require('main-bower-files');
+    var coffee = require('gulp-coffee');
+    var less = require('gulp-less');
+    var cssmin = require('gulp-minify-css');
+    var gulp = require('gulp');
 
-gulp.task('less', function() {
-    return gulp.src('${srcLess}/*.less')
-        .pipe(less())
-        .pipe(cssmin({root: '${srcLess}'}))
-        .pipe(gulp.dest('${destLess}'));
-});
+    gulp.task('less', function() {
+        return gulp.src('${srcLess}/*.less')
+            .pipe(less())
+            .pipe(cssmin({root: '${srcLess}'}))
+            .pipe(gulp.dest('${destLess}'));
+    });
 
-gulp.task('coffee', function() {
-    gulp.src('${srcCoffee}/*.coffee')
-        .pipe(coffee())
-        .pipe(uglify())
-        .pipe(gulp.dest('${destCoffee}'));
-});
+    gulp.task('coffee', function() {
+        gulp.src('${srcCoffee}/*.coffee')
+            .pipe(coffee())
+            .pipe(uglify())
+            .pipe(gulp.dest('${destCoffee}'));
+    });
 
-gulp.task('bower-files', function() {
-    if (fs.existsSync('bower.json')) {
-        gulp.src(mainBowerFiles(), { base: '${workDir}/bower_components' })
-            .pipe(gulp.dest('${destLib}'));
-    }
-});
+    gulp.task('bower-files', function() {
+        if (fs.existsSync('bower.json')) {
+            gulp.src(mainBowerFiles(), { base: '${workDir}/bower_components' })
+                .pipe(gulp.dest('${destLib}'));
+        }
+    });
 
-gulp.task('default', ['less', 'coffee', 'bower-files'], function() {
-});
+    gulp.task('default', ['less', 'coffee', 'bower-files'], function() {
+    });
+})();
