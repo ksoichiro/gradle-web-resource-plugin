0fcc9d70ce7d4a613ee8afa2b514aba5cc80671a
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-07-04 19:51:49 +0900

    Auto configure each npm dependencies.

--- plugin/src/main/groovy/com/github/ksoichiro/web/resource/NpmInstallWrapperTask.groovy
+++ plugin/src/main/groovy/com/github/ksoichiro/web/resource/NpmInstallWrapperTask.groovy
@@ -6,7 +6,6 @@ import groovy.json.JsonOutput
 
 class NpmInstallWrapperTask extends NpmTask {
     static final String NAME = 'npmInstallWrapper'
-    static final String DEFAULT_BOWER_VERSION = "1.3.12"
 
     NpmInstallWrapperTask() {
         setNpmCommand('install')
@@ -29,21 +28,25 @@ class NpmInstallWrapperTask extends NpmTask {
         }
         def npmConfig = extension.npm ? extension.npm.clone() as Map : [:]
         if (!npmConfig.containsKey('devDependencies')) {
-            npmConfig['devDependencies'] = [
-                    bower             : DEFAULT_BOWER_VERSION,
-                    gulp              : "3.8.11",
-                    "coffee-script"   : "1.9.1",
-                    "main-bower-files": "2.7.0",
-                    "gulp-less"       : "3.0.2",
-                    "gulp-minify-css" : "1.0.0",
-                    "gulp-filter"     : "2.0.2",
-                    "gulp-coffee"     : "2.3.1",
-                    "gulp-uglify"     : "0.2.1"
-            ]
+            npmConfig['devDependencies'] = [:] as Map
         }
-        if (!npmConfig.devDependencies.containsKey('bower')) {
-            npmConfig.devDependencies['bower'] = DEFAULT_BOWER_VERSION
+
+        [
+                "bower"           : "1.3.12",
+                "gulp"            : "3.8.11",
+                "coffee-script"   : "1.9.1",
+                "main-bower-files": "2.7.0",
+                "gulp-less"       : "3.0.2",
+                "gulp-minify-css" : "1.0.0",
+                "gulp-filter"     : "2.0.2",
+                "gulp-coffee"     : "2.3.1",
+                "gulp-uglify"     : "0.2.1"
+        ].each { name, version ->
+            if (!npmConfig.devDependencies.containsKey(name)) {
+                npmConfig.devDependencies[name] = version
+            }
         }
+
         new File(extension.workDir, 'package.json').text = JsonOutput.prettyPrint(JsonOutput.toJson(npmConfig))
         super.exec()
     }
