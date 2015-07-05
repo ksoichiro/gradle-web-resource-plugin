a213e3a9f32d7e2806a71555bfaf7dda8a0a5fbf
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-07-05 11:13:54 +0900

    Changed coffeeScript, less and lib to dynamic properties.

--- plugin/src/main/groovy/com/github/ksoichiro/web/resource/WebResourceExtension.groovy
+++ plugin/src/main/groovy/com/github/ksoichiro/web/resource/WebResourceExtension.groovy
@@ -11,25 +11,35 @@ class WebResourceExtension {
 
     Project project
     File workDir
-    WebResourceProcessor base
-    WebResourceProcessor coffeeScript
-    WebResourceProcessor less
-    WebResourceProcessor lib
+    Map resources
     Map npm
     Map bower
 
     WebResourceExtension(Project project) {
         this.project = project
         this.workDir = project.file("${this.project.buildDir}/webResource");
+        this.resources = [:]
         this.base = new WebResourceProcessor("src/main", "${this.project.buildDir.name}/webResource/outputs")
         this.coffeeScript = new WebResourceProcessor("coffee", "js")
         this.less = new WebResourceProcessor("less", "css")
         this.lib = new WebResourceProcessor(null, "lib")
     }
 
+    def propertyMissing(String name) {
+        if (resources.containsKey(name)) {
+            resources."$name"
+        }
+    }
+
+    def propertyMissing(String name, arg) {
+        resources.put(name, arg)
+    }
+
     def methodMissing(String name, def args) {
         if (this.metaClass.hasProperty(this, name)) {
             return ConfigureUtil.configure(args[0] as Closure, this."$name")
+        } else if (this.resources.containsKey(name)) {
+            return ConfigureUtil.configure(args[0] as Closure, this.resources."$name")
         } else {
             throw new GradleException("Missing method: ${name}")
         }
