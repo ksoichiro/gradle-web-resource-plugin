914cf6c601cbc8565a71b2bc6cbb1fe8c980985b
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-07-09 00:35:46 +0900

    Added generated bower.json as an output.

--- plugin/src/main/groovy/com/github/ksoichiro/web/resource/WebResourceInstallBowerDependenciesTask.groovy
+++ plugin/src/main/groovy/com/github/ksoichiro/web/resource/WebResourceInstallBowerDependenciesTask.groovy
@@ -5,20 +5,21 @@ import groovy.json.JsonOutput
 
 class WebResourceInstallBowerDependenciesTask extends NodeTask {
     static final String NAME = "webResourceInstallBowerDependencies"
+    WebResourceExtension extension
 
     WebResourceInstallBowerDependenciesTask() {
         dependsOn([WebResourceInstallDependenciesTask.NAME])
         this.project.afterEvaluate {
-            WebResourceExtension extension = this.project.webResource
+            extension = this.project.webResource
             getInputs().property('bower', extension.bower)
-            getOutputs().dir(new File(extension.workDir, 'bower_components'))
+            getOutputs().files(new File(extension.workDir, 'bower_components'), getBowerJson())
             setWorkingDir(extension.workDir)
         }
     }
 
     @Override
     void exec() {
-        def extension = project.webResource as WebResourceExtension
+        extension = project.webResource
         if (!extension.bower || !extension.bower.containsKey('dependencies')) {
             println "No bower config"
             return
@@ -31,9 +32,13 @@ class WebResourceInstallBowerDependenciesTask extends NodeTask {
         if (!bowerConfig.containsKey('name')) {
             bowerConfig['name'] = 'webResource'
         }
-        new File(extension.workDir, 'bower.json').text = JsonOutput.prettyPrint(JsonOutput.toJson(bowerConfig))
+        getBowerJson().text = JsonOutput.prettyPrint(JsonOutput.toJson(bowerConfig))
         setArgs(['install', '--config.interactive=false'])
 
         super.exec()
+    }
+
+    File getBowerJson() {
+        new File(extension.workDir, 'bower.json')
     }
 }
