6dfaed6cfcddada5ee394962cdbb934b724aedd0
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-10-12 00:11:52 +0900

    Add a test.

---
+++ plugin/src/test/groovy/com/github/ksoichiro/web/resource/FilterableProcessorSpec.groovy
@@ -0,0 +1,18 @@
+package com.github.ksoichiro.web.resource
+
+import spock.lang.Specification
+
+class FilterableProcessorSpec extends Specification {
+    def "init"() {
+        when:
+        def fp = new FilterableProcessor("src", "dest", ["**/*.less"], ["**/_*.less"])
+
+        then:
+        fp.src == "src"
+        fp.dest == "dest"
+        fp.include == ["**/*.less"]
+        fp.exclude == ["**/_*.less"]
+        fp.enabled
+        fp.minify
+    }
+}
