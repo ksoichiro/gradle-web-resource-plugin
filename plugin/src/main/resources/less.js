e235c6784182b82e43e7e66e5d982729ba687e98
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-01-30 18:59:08 +0900

    Replace async to q

--- plugin/src/main/resources/less.js
+++ plugin/src/main/resources/less.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var less = require('less');
-var async = require('async');
+var Q = require('q');
 var common = require('./common.js');
 
 var lessSrcPath = process.argv[2];
@@ -21,37 +21,41 @@ common.mkdirsIfNotExistSync(lessDestDir);
 lessConvert(lessSrcPath, lessSrcName, [path.dirname(lessSrcPath)], path.join(lessDestDir, lessSrcName.replace(/\.less/, '.css')));
 
 function lessConvert(filepath, filename, searchPaths, outputPath) {
-  var lessString = fs.readFileSync(filepath, 'utf8');
-  async.series([
-    function(callback) {
-      less.render(lessString,
-        {
-          paths: searchPaths,
-          filename: filename,
-          compress: minify
-        },
-        function (e, output) {
-          if (e) {
-            common.logE(logLevel, 'LESS: compilation failed: ' + e);
-            callback(e, "render");
-            return;
-          }
-          common.mkdirsIfNotExistSync(path.dirname(outputPath));
-          fs.writeFile(outputPath, output.css, function(err) {
-            if (err) {
-              common.logE(logLevel, 'LESS: saving file failed: ' + err);
-              callback(err, "render");
-            } else {
-              callback(null, "render");
-            }
-          });
-        });
-    }
-  ], function(err, results) {
-    if (err) {
-      exitCode = 1;
-    } else {
-      common.logI(logLevel, 'LESS: processed ' + filepath);
-    }
-  });
+  (function() {
+    var deferred = Q.defer();
+    var lessString = fs.readFileSync(filepath, 'utf8');
+    less.render(lessString,
+      {
+        paths: searchPaths,
+        filename: filename,
+        compress: minify
+      },
+      function (e, output) {
+        if (e) {
+          common.logE(logLevel, 'LESS: compilation failed: ' + e);
+          deferred.reject(e);
+        } else {
+          deferred.resolve(output);
+        }
+      });
+    return deferred.promise;
+  })()
+  .then(function(output) {
+    var deferred = Q.defer();
+    common.mkdirsIfNotExistSync(path.dirname(outputPath));
+    fs.writeFile(outputPath, output.css, function(err) {
+      if (err) {
+        common.logE(logLevel, 'LESS: saving file failed: ' + err);
+        deferred.reject(err);
+      } else {
+        common.logI(logLevel, 'LESS: processed ' + filepath);
+        deferred.resolve();
+      }
+    });
+    return deferred.promise;
+  })
+  .catch(function(error) {
+    exitCode = 1;
+  })
+  .done();
 }
